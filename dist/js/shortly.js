6596339166d17ba6673da32e48fc1aa230b29e0d
oktaviani ella karlina <oek@gmail.com>
2023-09-22 17:47:26 +0700

    edit shortly.html untuk drop down menu tampillan mobile

--- dist/js/shortly.js
+++ dist/js/shortly.js
@@ -11,3 +11,13 @@ function submitform(e){
         link_input.classList.add('border-red')
     }
 }
+
+const btn=document.getElementById('menu-btn');
+const menu=document.getElementById('menuMobile');
+btn.addEventListener('click',navToggle);
+
+function navToggle(){
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+}
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,8 +1,8 @@
-console.log("hello ellaaaa")
+console.log("index.js")
 import "./style.css"
 
 
-const btn=document.getElementById('menu-btn');
+const btn=document.getElementById('menu-loopstudio');
 const menu=document.getElementById('menu');
 btn.addEventListener('click',navToggle);
 
